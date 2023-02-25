b2ecf3f8a1e035ab8788c662f79d707c1999a434
NijatS <nijatjs@code.edu.az>
2023-02-25 11:44:23 +0400

    done

---
+++ testCs/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] numbers = new int[10];
+
+Console.WriteLine("Please fill the array : ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = (int)Convert.ToInt32(Console.ReadLine());
+}
+int count1 = 0, count2 = 0, sum1 = 0, sum2 = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        count1++;
+        sum1 += numbers[i];
+    }
+    else
+    {
+        count2++;
+        sum2 += numbers[i];
+    }
+}
+Console.WriteLine("Count of Odd Number : " + count1);
+Console.WriteLine("Count of Even Number : " + count2);
+Console.WriteLine("Sum of Odd Number : " + sum1);
+Console.WriteLine("Sum of Even Number : " + sum2);
+
+if(sum1 > sum2)
+{
+    Console.WriteLine("Sum of Odd Number more than other");
+}
+else
+{
+    Console.WriteLine("Sum of Even Number more than other");
+}
